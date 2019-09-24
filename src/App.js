01594c493754c452eb73f9333108d6151d4b4154
Filip Bendevski <bende@localhost.localdomain>
2019-09-24 22:02:07 +0800

    dis

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+    <h2 id="usr">User Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/1e935cee5ec7dbd266fc319c53850020-full.jpg" alt=""/></a>
+    <p>Allows for outright lazy parenting.</p>
+    <h2 id="inter">Interface Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/c51bd8b735639b190ecb106c477d15b0-full.jpg" alt=""/></a>
+    <p>This is a bad influence for me, not to mention children</p>
+    <h2 id="address">Address Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/8539fddf26cd82e1bd745c6e27c01d8b-full.jpg" alt=""/></a>
+    <p>Brings good and bad.</p>
+    <h2 id="city">City Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/5fb00115079548bd6150f8b34470909f-full.jpg" alt=""/></a>
+    <p>Connects to good and bad.</p>
+    <h2 id="cloud">Cloud Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/43d24a6d12e74b298b04f16fda788ebd-full.jpg" alt=""/></a>
+    <p>Stores good and bad.</p>
+    <h2 id="earth">Earth Layer</h2>
+    <a href="https://imggmi.com" target="_blank"><img src="https://cdn1.imggmi.com/uploads/2019/9/17/68303659d0abdc6b3bbbf2fa9e571480-full.jpg" alt=""/></a>
+    <p>Bad in this case, but can be good too.</p>
+    <h3 id="luis">Reference:</h3>
+    <p class="ref">Address layer img by Luis Hernan.</p>
+</body>
   );
 }
 
